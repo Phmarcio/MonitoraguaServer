ca1f5d9f6c01243ef5e17408af256c5cc0230df8
herlan <herlanassis@gmail.com>
2017-04-14 18:55:16 -0300

    implementando logica de historico e notificacao

--- gerenciar_abastecimento/models.py
+++ gerenciar_abastecimento/models.py
@@ -1,8 +1,29 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
+import datetime
 from django.db import models
 from django.utils import timezone
 from django.utils.encoding import python_2_unicode_compatible
+
+
+def mudar_status_setor(pk):
+    setores = Setor.objects.all()
+
+    for setor in setores:
+        if setor.id != pk:
+            print "setor com o status para false: " + setor.nome
+
+            setor.em_abastecimento = False
+            historico_atual = setor.historicos.get_queryset().last()
+            historico_atual.data_fim = timezone.now()
+            historico_atual.save()
+
+            bairros = setor.bairros.get_queryset()
+            for bairro in bairros:
+                bairro.em_abastecimento = False
+                bairro.save()
+
+            setor.save()
 
 
 # Create your models here.
@@ -19,12 +40,39 @@ class Setor(models.Model):
     def __str__(self):
         return self.nome
 
+    def noticar_abastecimento(self):
+        historico_atual = self.historicos.get_queryset().last()
+
+        if historico_atual:
+            if historico_atual.data_fim:
+                # Se o último historico já tiver data de fim ele cria um novo
+                if (timezone.now() - datetime.timedelta(days=3)).date() > historico_atual.data_fim:
+                    # se não existir ele cria um novo e muda o status dos demais setores
+                    historico_atual = self.historicos.create()
+                else:
+                    return "Este setor foi abastecido no dia %s ao dia %s" % (
+                        historico_atual.data_inicio.strftime("%d/%m/%Y"),
+                        historico_atual.data_fim.strftime("%d/%m/%Y"))
+        else:
+            # se não existir ele cria um novo e muda o status dos demais setores
+            historico_atual = self.historicos.create()
+
+        historico_atual.qts_abastecimentos += 1
+        historico_atual.save()
+        if historico_atual.qts_abastecimentos >= 5:
+            self.em_abastecimento = True
+            #  muda o estados de de abastecimento para falso e altera o status de cada bairro
+            mudar_status_setor(self.id)
+        self.save()
+
+        return "Obrigado por notificar!"
+
 
 @python_2_unicode_compatible
 class Bairro(models.Model):
     nome = models.CharField("nome", max_length=128, blank=False, null=False)
     em_abastecimento = models.BooleanField("Em abastecimento?", default=False)
-    setor = models.ForeignKey(Setor, on_delete=models.PROTECT)
+    setor = models.ForeignKey(Setor, related_name='bairros', on_delete=models.PROTECT)
 
     class Meta:
         verbose_name = 'bairro'
@@ -33,6 +81,16 @@ class Bairro(models.Model):
 
     def __str__(self):
         return self.nome
+
+    def get_abastecido(self):
+        resposta = self.setor.noticar_abastecimento()
+
+        if not (self.em_abastecimento):
+            if self.setor.em_abastecimento:
+                self.em_abastecimento = True
+                self.save()
+
+        return resposta
 
 
 @python_2_unicode_compatible
